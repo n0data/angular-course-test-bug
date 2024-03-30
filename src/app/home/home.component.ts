f30bfdbe42bd4c21dea8dc7ac5c0b43478b7dddb
n0data <sasic.vladimir2@gmail.com>
2024-03-31 00:36:41 +0100

    removed ssr

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,6 +14,25 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
       this.loadData();
 
+
+
+    
+      // let numStr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      
+      // const sumEvens = (numStr) => {
+      //   let sum = 0;
+      //   for (let i = 0; i < numStr.length; i++) {
+      //     if (numStr[i] % 2 === 0) {
+      //     sum = sum + numStr[i];
+      //     }
+      //   }
+      //   return sum;
+      // }
+       
+      // console.log(sumEvens(numStr));
+     
+
+
   }
 
 
